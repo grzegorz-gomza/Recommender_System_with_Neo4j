0891a99597adc2c202c3e5ef0ab965d8c2b2b815
Grzegorz Gomza <gomza.datascience@gmail.com>
2025-09-22 17:47:24 +0200

    changed gpt to leaatest version gpt 5 nano

--- src/chat/llm.py
+++ src/chat/llm.py
@@ -5,11 +5,8 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 llm = ChatOpenAI(
     openai_api_key=st.secrets["OPENAI_API_KEY"],
     model=st.secrets["OPENAI_MODEL"],
-    temperature=1,  # must be 1 for some models
-    max_tokens=2000,
-    stop=None,            # explicitly clear stop
-).bind(stop=None)         # also clear in runtime bindings
-
+    temperature=1,
+)
 
 # Create the Embedding model
 embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
