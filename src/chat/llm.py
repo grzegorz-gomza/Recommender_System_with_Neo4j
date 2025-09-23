a922ded1a8aee1a21c71e94621a6f2c8e403bd4e
Grzegorz Gomza <gomza.datascience@gmail.com>
2025-09-23 10:10:48 +0200

    changed gpt to leaatest version gpt 5 nano

--- src/chat/llm.py
+++ src/chat/llm.py
@@ -1,13 +1,11 @@
 import streamlit as st
-# from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 # Create the LLM
 llm = ChatOpenAI(
     openai_api_key=st.secrets["OPENAI_API_KEY"],
     model=st.secrets["OPENAI_MODEL"],
-    temperature=1,
+    temperature=0.5,
 )
 
 # Create the Embedding model
